8d423d2675d392db05b7e220c19a318d33eaa4c2
malan88 <malan88@standingwater.io>
2020-06-08 20:42:10 -0400

    Expand skeleton to include scheduler, argparse
    I've coded in the dynamism of the interval, but I haven't coded in the
    permissible texting hours (or even the actual texting, though the
    capability is there).
    
    I want (a) permissible hours (who wants a text at midnight), (b) a save
    feature, (c) output, (d) the ability to read from argparse rather than
    input dialog, and (e) a configuration yaml file.
    
    That should pretty much cover it.

--- vote.py
+++ vote.py
@@ -1,4 +1,9 @@
 import os
+import argparse
+import time
+
+from sched import scheduler
+import datetime as dt
 
 from twilio.rest import Client
 
@@ -10,17 +15,52 @@ FROM_PHONE = os.environ['TWILIO_PHONE']
 # Client persists
 CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
 
+# Frequency dictionary keyed by months to go
+FREQUENCY = {
+    1: 15 * 60,         # 15 minutes
+    2: 30 * 60,         # 30 minutes
+    3: 60 * 60,         # hour
+    4: 2 * 60 * 60,     # 2 hours
+    5: 4 * 60 * 60,     # 4 hours
+    6: 8 * 60 * 60,     # 8 hours
+}
+
 def sendtext(to, body):
     message = CLIENT.messages.create(to=to, from_=FROM_PHONE, body=body)
     return message.sid
 
-def main():
+def runschedule(duedate, to, body):
+    s = scheduler(time.time, time.sleep)
+
+    # get interval from duedate - now
+    timetogo = duedate - dt.datetime.now()
+    interval = FREQUENCY[6]
+    months = timetogo.days / 30
+    for months, value in reversed(FREQUENCY).items():
+        if timetogo.days // 30 <= months
+            interval = value
+
+    s.enter(interval, 1, runschedule, argument=(duedate, to, body))
+    s.run()
+
+def main(args):
     # Get to and body as inputs
     to = '+1' + input("Phone # to text (5558675309): ")
     body = input("Message to send: ")
+    duedate = input("Due date (YYYY-MM-DD): ")
+    duedate = dt.datetime(*[int(num) for num in duedate.split('/|-|.')])
 
-    message_id = sendtext(to, body)
-    print(message_id)
+    if not args.test:
+        runschedule(duedate, to, body)
+        print(message_id)
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser("""Get your friend to register to vote by
+                                     constantly texting them.""")
+    parser.add_argument('-r', '--resume', action='store_true', help="""Resume
+                        former texting campaign. Requires a json file.""")
+    parser.add_argument('-t', '--test',  action='store_true', help="""Test the
+                        system, don't actually send a text.""")
+    args = parser.parse_args()
+    main(args)
